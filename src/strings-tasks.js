3d3a48b3e696a7b341fad10e6800719742d4f14d
Oleksandr Murga <sanay8myrga@gmail.com>
2025-03-08 20:09:33 +0200

    feat: решено 6. осталось 37

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -71,8 +71,9 @@ function concatenateStrings(value1, value2) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  const char = value.charAt(0);
+  return char;
 }
 
 /**
