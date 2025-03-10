d24fa124d8fec94efea74dd760141faaaba2025b
Oleksandr Murga <sanay8myrga@gmail.com>
2025-03-10 14:39:47 +0200

    feat: решено 16. осталось 27

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -155,8 +155,10 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const result = str.indexOf(value);
+  if (result === -1) return str;
+  return str.slice(0, result) + str.slice(result + value.length);
 }
 
 /**
